b40af343f0a1b4f462ae9c0c67bd4004491af011
hungbui7690 <hungbui7690@gmail.com>
2023-03-26 05:37:01 +0700

    Dealing with Numbers

--- playground/index.js
+++ playground/index.js
@@ -1,6 +1,11 @@
 /*
-  Are These Commands Even Useful
-  - pic: useful 
+  Dealing with Numbers
+  - pic
+
+  - INCR / DECR: add or subtract 1
+  - INCRBY / DECRBY: we can specify the amount we want to add or subtract
+  - INCRBYFLOAT
+    > if we want to decrease > just need to add negative sign 
 
 
 */
