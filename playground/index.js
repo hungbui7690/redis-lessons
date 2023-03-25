04c787cdf41351d7f1ccd39ea19c73eb9775461d
hungbui7690 <hungbui7690@gmail.com>
2023-03-26 06:24:01 +0700

    Exercise

--- playground/index.js
+++ playground/index.js
@@ -1,11 +1,11 @@
 /*
-  Dealing with Numbers
-  - pic
+  Using a Completed Notebook
+  - BasicStringsCompleted.json > upload and use
 
-  - INCR / DECR: add or subtract 1
-  - INCRBY / DECRBY: we can specify the amount we want to add or subtract
-  - INCRBYFLOAT
-    > if we want to decrease > just need to add negative sign 
+////////////////////////////////////////
+
+  Exercise
+  - StringsExercises.json
 
 
 */
