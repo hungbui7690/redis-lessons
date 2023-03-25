1b9ee5885b9bec839b20a22bda6029832a4d7ffc
hungbui7690 <hungbui7690@gmail.com>
2023-03-26 05:17:51 +0700

    GET and MGET

--- playground/index.js
+++ playground/index.js
@@ -1,10 +1,6 @@
 /*
-  Setting Multiple Keys
-  - SET with NX === SETNX
-  - SET with EX = SETEX
-
-  - MSET: set multiple keys at the same time
+  GET and MGET
 
 */
 
-const rBook = 'http://localhost:3050/'
+const rBook = 'http://localhost:3050/' // npx rbook
