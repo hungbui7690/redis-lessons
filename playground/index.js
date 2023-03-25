a1b9cdefb93b2824ecd106724e7e0e713e6b56b6
hungbui7690 <hungbui7690@gmail.com>
2023-03-26 05:15:40 +0700

    Setting Multiple Keys

--- playground/index.js
+++ playground/index.js
@@ -1,15 +1,9 @@
 /*
-  Use Case of Expiration Options
-  - pic: expiration
+  Setting Multiple Keys
+  - SET with NX === SETNX
+  - SET with EX = SETEX
 
-  - some options: 
-    + EX: seconds
-    + PX: milliseconds
-    + EXAT / PXAT: specific date/time
-    + KEEPTTL: keep any expiration that has already been applied to this key
-
-  - why we want to use this option
-    + pic
+  - MSET: set multiple keys at the same time
 
 */
 
