0c5d6b56c9c2b6fa668ec04c2dd0866fc5b79bc1
hungbui7690 <hungbui7690@gmail.com>
2023-03-26 04:40:43 +0700

    Variations of SET

--- playground/index.js
+++ playground/index.js
@@ -1,13 +1,24 @@
 /*
-  Basic Commands
-  - connection URL: 
-    > redis://:lQhQSul4e7reThEBgKtzRFTxlgDH1Iww@redis-12581.c241.us-east-1-4.ec2.cloud.redislabs.com:12581
-
+  Documentation on Commands
   - pic
-  - SET message 'Hi There'   
-    > after the value > there should be no space > otherwise, error
-    > command: { name: 'set', args: [ 'message', 'Hi There', '' ] } > this is the case when we have space after "Hi There" > will create 3rd argument
+  - some commands will have the same behavior :
+    + GET
+    + GETEX
+    + GETDEL
 
-  - pic: depends on the data structure, we will use different commands
+
+  - we will need to refers to docs so much 
+    > https://redis.io/commands/
+
+
+//////////////////////////////////////
+
+  Variations of SET
+  - NX | XX: only set this key if: (there are 2 cases > NX | XX): 
+    + key exists
+    + key does not exist
+
+  (***) we need to check rbook in each lesson 
+        > http://localhost:3050/
 
 */
