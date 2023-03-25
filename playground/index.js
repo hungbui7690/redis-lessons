69b3f56a5110a7646d1a2367e85d8f43110844e7
hungbui7690 <hungbui7690@gmail.com>
2023-03-26 03:39:52 +0700

    Why Use Redis & Initial Setup: Setup rbook

---
+++ playground/index.js
@@ -0,0 +1,26 @@
+/*
+  Why Use Redis
+  - pic
+
+///////////////////////////////////////
+
+  Initial Setup
+  - pic
+  - create account on redis.com
+    + create free subscription
+    + create db > Active Db
+
+///////////////////////////////////////
+
+  Run rbook on cloud
+  - to query > use rbook.cloud
+    > Connect > we just need host, port + password (pic)
+
+///////////////////////////////////////
+
+  Run rbook locally
+  > npx rbook
+  > http://localhost:3050/
+
+
+*/
