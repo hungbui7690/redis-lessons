7b0ca593cbf28c7b1e3943fd05159bbe85b6ff9d
hungbui7690 <hungbui7690@gmail.com>
2023-03-26 08:11:24 +0700

    Client Lib, Redis DB Design, Naming & Adding Page Caching P1

--- playground/src/app.js
+++ playground/src/app.js
@@ -1,8 +1,34 @@
 /*
-  Project Setup
+  Redis Client Libraries
   - pic
-    + install libs
-    + setup env file 
-    + npm run dev
+  - we need to focus on 2 folders:
+    + services/queries 
+    + services/redis
+
+  - use node-redis package to connect/query redis db: 
+    > https://github.com/redis/node-redis
+      > no docs for this one 
+
+///////////////////////////////////////
+
+  First Implementation Task
+  - pic
+
+///////////////////////////////////////
+
+  Redis Design Methodology
+  - pic
+
+///////////////////////////////////////
+
+  Key Naming Methodology
+  - pic 
+
+///////////////////////////////////////
+
+  Adding Page Caching P1
+  - services/queries/page-cache.ts
+
+
 
 */
