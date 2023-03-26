290c88cb218087aca847a1a361e5cf45d505a3d8
hungbui7690 <hungbui7690@gmail.com>
2023-03-26 08:21:11 +0700

    Adding Page Caching P2

--- playground/src/app.js
+++ playground/src/app.js
@@ -1,34 +1,7 @@
 /*
-  Redis Client Libraries
-  - pic
-  - we need to focus on 2 folders:
-    + services/queries 
-    + services/redis
-
-  - use node-redis package to connect/query redis db: 
-    > https://github.com/redis/node-redis
-      > no docs for this one 
-
-///////////////////////////////////////
-
-  First Implementation Task
-  - pic
-
-///////////////////////////////////////
-
-  Redis Design Methodology
-  - pic
-
-///////////////////////////////////////
-
-  Key Naming Methodology
-  - pic 
-
-///////////////////////////////////////
-
-  Adding Page Caching P1
+  Adding Page Caching P2
+  - /services/redis/client.ts > how to connect to redis db
   - services/queries/page-cache.ts
 
-
-
+  > after finish this lesson, go to website /privacy, /about, /signin, /signup
 */
--- playground/src/services/queries/page-cache.ts
+++ playground/src/services/queries/page-cache.ts
@@ -1,3 +1,21 @@
-export const getCachedPage = (route: string) => {};
+import { client } from '../redis/client';
 
-export const setCachedPage = (route: string, page: string) => {};
+// *** list of all routes that we want to cache
+const cacheRoutes = ['/about', '/privacy', '/auth/signin', '/auth/signup'];
+
+export const getCachedPage = (route: string) => {
+	// check if the route we want to access is in the cache list or not
+	if (cacheRoutes.includes(route)) {
+		client.get('pagecache#' + route); // get the cache version
+	}
+
+	return null;
+};
+
+export const setCachedPage = (route: string, page: string) => {
+	if (cacheRoutes.includes(route)) {
+		return client.set('pagecache#' + route, page, {
+			EX: 2 // expire in 2s, just to test > in real app > maybe 1 or 2 days
+		});
+	}
+};
